0007591319ac6b078de7de890fa982a7268ef88b
igor-davidyuk <igor.davidyuk@intel.com>
2023-03-08 16:18:26 +0200

    functioning resnet18 example
    Signed-off-by: igor-davidyuk <igor.davidyuk@intel.com>

--- pt_based_solution/model_interface.py
+++ pt_based_solution/model_interface.py
@@ -1,42 +1,53 @@
 from collections import defaultdict
 from copy import deepcopy
 import numpy as np
+from scipy.optimize import linear_sum_assignment
 import torch
 
 
-class ModelSupportingPermutations(torch.nn.module):
-    def __init__(self) -> None:
+class ModelSupportingPermutations:
+    def __init__(self, model_id: int, ) -> None:
+        self.model_id = model_id
         self.model = self.get_model()
         # This is {'weight_name': (P1, P2, ...)]}
         self._weight_name_to_perm_vector = None
         # This is {'P_name': ('weight_name', target_axis)}
-        self._perm_name_to_weight_name = None
+        self._perm_name_to_weight_names = None
 
     def get_model(self):
         raise NotImplementedError
 
+    def train(self):
+        raise NotImplementedError
+
+    def validate(self):
+        raise NotImplementedError
+
     @property
     def weight_name_to_perm_vector(self):
-        "Get Weight Name to Permutation Vector dict."
+        """
+        Get Weight Name to Permutation Vector dict.
+
+        This method should be automated in a real product.
+        """
         if self._weight_name_to_perm_vector is None:
             raise NotImplementedError
 
         return self._weight_name_to_perm_vector
 
     @property
-    def perm_name_to_weight_name(self):
+    def perm_name_to_weight_names(self):
         "Get Permutation Name to Weight Name and Target Axis dict."
-        if self._perm_name_to_weight_name is None:
-            self._perm_name_to_weight_name = defaultdict(list)
+        if self._perm_name_to_weight_names is None:
+            self._perm_name_to_weight_names = defaultdict(list)
             for w_name, perm_vector in self.weight_name_to_perm_vector.items():
                 for axis, perm_name in enumerate(perm_vector):
                     if perm_name is not None:
-                        self._perm_name_to_weight_name[perm_name].append
-                        (
+                        self._perm_name_to_weight_names[perm_name].append(
                             (w_name, axis)
                         )
 
-        return self._perm_name_to_weight_name
+        return self._perm_name_to_weight_names
 
     @property
     def state_dict(self):
@@ -57,18 +68,82 @@ class ModelSupportingPermutations(torch.nn.module):
         new_model._apply_permutation(permutations)
         return new_model
 
-    def get_blend_model(self, admixture_model, fraction):
+    def get_blend_model(self, admixture_model, fraction, skip_not_permuted=False):
         self_state = self.state_dict
         admixture_state = admixture_model.state_dict
         blend_state = {}
         for name in self_state:
-            blend_state[name] = (
-                    admixture_state[name] * fraction +
-                    self_state[name] * (1 - fraction)
-                )
+            if name in self.weight_name_to_perm_vector:
+                blend_state[name] = (
+                        admixture_state[name] * fraction +
+                        self_state[name] * (1 - fraction)
+                    )
+            else:
+                if not skip_not_permuted:
+                    blend_state[name] = self_state[name]
+                else:
+                    raise KeyError(
+                        f'Permutation for weight {name} is not defined.')
         new_model = deepcopy(self)
         new_model.state_dict = blend_state
         return new_model
+
+    def match_weights(
+            self, model_b, max_iter=100, init_permutations=None, seed: int = 1
+                     ) -> dict[str, np.ndarray]:
+        """Find a permutation of `model_b` to make them match self weights."""
+        assert isinstance(model_b, self.__class__)
+
+        # Prepare a random number generator
+        rng = np.random.default_rng(seed)
+
+        def rngmix(seed):
+            return np.random.default_rng(
+                [rng._bit_generator._seed_seq.entropy, hash(seed)])
+
+        self_state = self.state_dict
+        model_b_state = model_b.state_dict
+        perm_sizes = {
+            perm_name: self_state[w_name_axis_list[0][0]].shape[w_name_axis_list[0][1]]
+            for perm_name, w_name_axis_list in self.perm_name_to_weight_names.items()
+            }
+
+        if init_permutations is not None:
+            permutations = init_permutations
+        else:
+            permutations = {perm_name: np.arange(perm_size)
+                            for perm_name, perm_size in perm_sizes.items()}
+        perm_names = list(permutations.keys())
+
+        for iteration in range(max_iter):
+            progress = False
+            for p_ix in rngmix(iteration).permutation(len(perm_names)):
+                perm_name = perm_names[p_ix]
+                perm_size = perm_sizes[perm_name]
+                A = np.zeros((perm_size, perm_size))
+                # We iterate over all the (weight_name, axis) pairs
+                # for a specific permutation matrix.
+                for weight_name, axis in self.perm_name_to_weight_names[perm_name]:
+                    w_a = self_state[weight_name]
+                    w_b = self._get_permuted_tensor(permutations, weight_name, model_b_state, skip_axis=axis)
+                    w_a = np.moveaxis(w_a, axis, 0).reshape((perm_size, -1))
+                    w_b = np.moveaxis(w_b, axis, 0).reshape((perm_size, -1))
+                    A += w_a @ w_b.T
+
+                ri, ci = linear_sum_assignment(A, maximize=True)
+                assert (ri == np.arange(len(ri))).all()
+
+                oldL = np.vdot(A, np.eye(perm_size)[permutations[perm_name]])
+                newL = np.vdot(A, np.eye(perm_size)[ci, :])
+                print(f"{iteration}/{perm_name}: {newL - oldL}")
+                progress = progress or newL > oldL + 1e-12
+
+                permutations[perm_name] = np.array(ci)
+
+            if not progress:
+                break
+
+        return permutations
 
     def _apply_permutation(self, permutations):
         weight_dict = self.state_dict
@@ -83,6 +158,8 @@ class ModelSupportingPermutations(torch.nn.module):
             self, permutations, weight_name, weight_dict, skip_axis=None
             ):
         weight = weight_dict[weight_name]
+        if weight_name not in self.weight_name_to_perm_vector:
+            return weight
         perm_vector = self.weight_name_to_perm_vector[weight_name]
         if perm_vector is None:
             return weight
@@ -96,6 +173,3 @@ class ModelSupportingPermutations(torch.nn.module):
                 weight = np.take(weight, permutations[perm_name], axis=axis)
 
         return weight
-
-    def forward(self, x):
-        return self.model.forward(x)
---
+++ pt_based_solution/resnet18_cifar10.py
@@ -0,0 +1,153 @@
+from model_interface import ModelSupportingPermutations
+import numpy as np
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+from torchvision.models import resnet18
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Subset
+from torch.optim import SGD
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+torch.use_deterministic_algorithms(False)
+np.random.seed(0)
+
+
+class RN18CIFAR(ModelSupportingPermutations):
+    def __init__(self, model_id: int) -> None:
+        assert 0 <= model_id <= 1, "Supporting two models"
+        torch.manual_seed(model_id)
+
+        super().__init__(model_id)
+
+        self.valid_dataset = None
+
+    def get_model(self):
+        return resnet18(num_classes=10)
+
+    def train(self, bs=512, lr=5e-3, epoches=20, device=None):
+        if device is None:
+            device = torch.device(
+                'cuda' if torch.cuda.is_available() else 'cpu'
+                )
+        train_transform = transforms.Compose([
+            transforms.RandomAffine(0, scale=[0.8, 1.2]),
+            transforms.RandomCrop(32, pad_if_needed=True),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomAffine(30),
+            transforms.ToTensor()
+        ])
+        train_dataset = CIFAR10(
+            '.', train=True, transform=train_transform, download=True)
+        indices = np.random.permutation(len(train_dataset))
+        # Splitting datasets in two parts
+        indices = np.array_split(indices, 2)[self.model_id]
+        train_dataset = Subset(train_dataset, indices)
+        train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True)
+
+        optimizer = SGD(self.model.parameters(), momentum=0.9, lr=lr)
+        scheduler = CosineAnnealingLR(optimizer, T_max=epoches, eta_min=1e-6)
+        loss_fn = nn.CrossEntropyLoss()
+
+        self.model.to(device)
+        for epoch in range(epoches):
+            self.model.train()
+            for data, target in train_loader:
+                optimizer.zero_grad()
+                data, target = data.to(device), target.to(device)
+                outputs = self.model(data)
+                loss = loss_fn(outputs, target)
+                loss.backward()
+                optimizer.step()
+            scheduler.step()
+
+            # Reporting on epoch results
+            train_loss = loss.item()
+            acc, val_loss = self.validate(device, loss_fn)
+            print(f'Epoch {epoch+1}:\t'
+                  f'Train loss: {train_loss:4f}\t'
+                  f'Test loss: {val_loss:4f}\t'
+                  f'Test accuracy: {acc:4f}')
+
+    def validate(self, device=None, loss_fn=None):
+        if device is None:
+            device = torch.device(
+                'cuda' if torch.cuda.is_available() else 'cpu'
+                )
+
+        if not self.valid_dataset:
+            valid_transform = transforms.Compose([
+                transforms.ToTensor()
+            ])
+            self.valid_dataset = CIFAR10(
+                '.', train=False, transform=valid_transform, download=True)
+        valid_loader = DataLoader(self.valid_dataset, batch_size=128)
+
+        self.model.eval()
+
+        total_samples = 0
+        val_score = 0
+        for data, target in valid_loader:
+            data, target = data.to(device), target.to(device)
+            outputs = self.model(data)
+            pred = outputs.argmax(dim=1)
+            val_score += pred.eq(target).sum().cpu().numpy()
+            total_samples += len(target)
+        accuracy = val_score / total_samples
+        if loss_fn is not None:
+            loss = loss_fn(outputs, target).item()
+        else:
+            loss = -1
+            print('(Accuracy, Loss=-1)')
+        return accuracy, loss
+
+    @property
+    def weight_name_to_perm_vector(self):
+        "Get Weight Name to Permutation Vector dict."
+        if self._weight_name_to_perm_vector is None:
+            conv = lambda name, p_in, p_out: {f"{name}.weight": (p_out, p_in, None, None)}
+            norm = lambda name, p: {f"{name}.weight": (p, ), f"{name}.bias": (p, )}
+            dense = lambda name, p_in, p_out: {f"{name}.weight": (p_out, p_in), f"{name}.bias": (p_out, )}
+
+            # This is for easy blocks that use a residual connection, without any change in the number of channels.
+            easyblock = lambda name, p: {
+                **conv(f"{name}.conv1", p, f"P_{name}_inner"),
+                **norm(f"{name}.bn1", f"P_{name}_inner"),
+                **conv(f"{name}.conv2", f"P_{name}_inner", p),
+                **norm(f"{name}.bn2", p)
+            }
+
+            # This is for blocks that use a residual connection, but change the number of channels via a Conv.
+            shortcutblock = lambda name, p_in, p_out: {
+                **conv(f"{name}.conv1", p_in, f"P_{name}_inner"),
+                **norm(f"{name}.bn1", f"P_{name}_inner"),
+                **conv(f"{name}.conv2", f"P_{name}_inner", p_out),
+                **norm(f"{name}.bn2", p_out),
+                **conv(f"{name}.downsample.0", p_in, p_out),
+                **norm(f"{name}.downsample.1", p_out),
+            }
+            self._weight_name_to_perm_vector = {
+                **conv("conv1", None, "P_bg0"),
+                **norm("bn1", "P_bg0"),
+                #
+                **easyblock("layer1.0", "P_bg0"),
+                **easyblock("layer1.1", "P_bg0"),
+                #
+                **shortcutblock("layer2.0", "P_bg0", "P_bg1"),
+                **easyblock("layer2.1", "P_bg1"),
+
+                **shortcutblock("layer3.0", "P_bg1", "P_bg2"),
+                **easyblock("layer3.1", "P_bg2"),
+
+                **shortcutblock("layer4.0", "P_bg2", "P_bg3"),
+                **easyblock("layer4.1", "P_bg3"),
+                #
+                **dense("fc", "P_bg3", None),
+            }
+
+        return self._weight_name_to_perm_vector
+
+
+if __name__ == "__main__":
+    model = RN18CIFAR(model_id=0)
+    model.train()
